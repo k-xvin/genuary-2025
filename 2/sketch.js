b8d7fe6da21a0348e081bef5461d48003fa1e3b4
Kevin Xu <xu@kxvin.com>
2026-05-15 21:10:36 -0700

    Refactor splotcher

--- 2/sketch.js
+++ 2/sketch.js
@@ -16,46 +16,43 @@ function setup() {
   createCanvas(800, 800);
   noLoop();
 
-  let bgColor = color(palette[3]);
-  background(bgColor);
+  background(color(palette[3]));
 
-  drawTextureLayers();
-}
-
-function drawTextureLayers() {
-  let numLayers = random(10, 20);
-
+  let numLayers = floor(random(10, 20));
   for (let i = 0; i < numLayers; i++) {
-    drawSplotchTexture();
+    let numSplotches = floor(random(4, 10));
+    for (let j = 0; j < numSplotches; j++) {
+      drawSplotch();
+    }
   }
 }
 
-function drawSplotchTexture() {
-  let numSplotches = floor(random(4, 10));
+function drawSplotch() {
+  let cx = random(width);
+  let cy = random(height);
+  let radius = random(80, 300);
+  let noiseScale = random(0.02, 0.04);
+  let numDots = floor(random(2000, 5000));
 
-  for (let s = 0; s < numSplotches; s++) {
-    let cx = random(width);
-    let cy = random(height);
-    let radius = random(80, 300);
-    let paletteColor = color(random(palette));
-    let dotColor = color(red(paletteColor) * 0.7, green(paletteColor) * 0.7, blue(paletteColor) * 0.7, random(30, 80));
+  let baseColor = color(random(palette));
+  let dotColor = color(
+    red(baseColor) * 0.7,
+    green(baseColor) * 0.7,
+    blue(baseColor) * 0.7,
+    random(30, 80)
+  );
 
-    let noiseScale = random(0.02, 0.04);
-    let numDots = floor(random(2000, 5000));
+  noStroke();
+  fill(dotColor);
 
-    noStroke();
-    for (let i = 0; i < numDots; i++) {
-      let angle = random(TWO_PI);
-      let r = random(radius);
-      let x = cx + r * cos(angle);
-      let y = cy + r * sin(angle);
-      let n = noise(x * noiseScale, y * noiseScale);
+  for (let i = 0; i < numDots; i++) {
+    let angle = random(TWO_PI);
+    let r = random(radius);
+    let x = cx + r * cos(angle);
+    let y = cy + r * sin(angle);
 
-      if (n > random(0.3, 0.55)) {
-        fill(dotColor);
-        let dotSize = random(1, 3);
-        ellipse(x, y, dotSize, dotSize);
-      }
+    if (noise(x * noiseScale, y * noiseScale) > random(0.3, 0.55)) {
+      ellipse(x, y, random(1, 3));
     }
   }
 }
