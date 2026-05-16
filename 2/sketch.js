27e32f46d7c0ce671b3e2fc6ed6df042803f0c51
Kevin Xu <xu@kxvin.com>
2026-05-15 21:14:30 -0700

    Interesting mistake, the perlin augmented line texture makes it look very organic

--- 2/sketch.js
+++ 2/sketch.js
@@ -18,21 +18,20 @@ function setup() {
 
   background(color(palette[3]));
 
-  let numLayers = floor(random(10, 20));
-  for (let i = 0; i < numLayers; i++) {
-    let numSplotches = floor(random(4, 10));
-    for (let j = 0; j < numSplotches; j++) {
-      drawSplotch();
-    }
+  let numSplotches = floor(random(40, 80));
+  for (let i = 0; i < numSplotches; i++) {
+    drawSplotch();
   }
 }
 
-function drawSplotch() {
-  let cx = random(width);
-  let cy = random(height);
-  let radius = random(80, 300);
-  let noiseScale = random(0.02, 0.04);
+function drawSplotch(cx, cy) {
+  cx = cx || random(width);
+  cy = cy || random(height);
+  let baseRadius = random(80, 300);
+  let blobNoiseScale = 0.02;
+  let blobNoiseOffset = random(1000);
   let numDots = floor(random(2000, 5000));
+  let dotNoiseScale = random(0.02, 0.04);
 
   let baseColor = color(random(palette));
   let dotColor = color(
@@ -47,12 +46,18 @@ function drawSplotch() {
 
   for (let i = 0; i < numDots; i++) {
     let angle = random(TWO_PI);
-    let r = random(radius);
+    let noiseVal = noise(
+      cos(angle) * blobNoiseScale + blobNoiseOffset,
+      sin(angle) * blobNoiseScale + blobNoiseOffset
+    );
+    let r = map(noiseVal, 0, 1, baseRadius * 0.3, baseRadius * 1.4);
     let x = cx + r * cos(angle);
     let y = cy + r * sin(angle);
 
-    if (noise(x * noiseScale, y * noiseScale) > random(0.3, 0.55)) {
-      ellipse(x, y, random(1, 3));
+    if (x > 0 && x < width && y > 0 && y < height) {
+      if (noise(x * dotNoiseScale, y * dotNoiseScale) > random(0.3, 0.55)) {
+        ellipse(x, y, random(1, 3));
+      }
     }
   }
 }
